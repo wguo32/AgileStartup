940fdc6439feb07862618ec478e0598a269f0d4c
wguo <vicre.guo@gmail.com>
2015-09-12 16:17:18 +0800

    util: add StringUtile : add File2String function

---
+++ com.datascilab.util/src/main/java/com/datascilab/util/common/StringUtil.java
@@ -0,0 +1,67 @@
+package com.datascilab.util.common;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import com.google.common.base.Strings;
+
+public class StringUtil {
+
+	private static class StringUtilHolder {
+
+		private static final StringUtil INSTANCE = new StringUtil();
+	}
+
+	public static StringUtil getInstance() {
+		return StringUtilHolder.INSTANCE;
+	}
+
+	public String fileToString(String fileName, String encoding) {
+		String rtn = null;
+		File file = new File(fileName);
+		if (file.exists() && file.isFile()) {
+			rtn = fileToString(file, encoding);
+		}
+
+		return rtn;
+
+	}
+
+	public String fileToString(File file, String encoding) {
+		
+		
+		if (file != null) {
+			if (!file.exists() || (file.exists() && !file.isFile())) {
+				return null;
+			}
+		} else {
+			return null;
+		}
+
+		if (Strings.isNullOrEmpty(encoding)) {
+			encoding = "utf-8";
+		}
+		Long filelength = file.length();
+		byte[] filecontent = new byte[filelength.intValue()];
+		try {
+			FileInputStream in = new FileInputStream(file);
+			in.read(filecontent);
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			return new String(filecontent, encoding);
+		} catch (UnsupportedEncodingException e) {
+			System.err.println("The OS does not support " + encoding);
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
